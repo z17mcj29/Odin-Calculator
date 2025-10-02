cfe01a1c9cb34d4228f39317f853bd9e7c7414f0
Michael Johnson <z17.mcj@gmail.com>
2025-10-02 01:47:55 -0600

    applied logic through step 3

---
+++ script.js
@@ -0,0 +1,25 @@
+function add(a, b){
+    return a + b;
+}
+function subtract(a, b){
+    return a - b;
+}
+function multiply(a,b){
+    return a * b;
+}
+function divide(a,b){
+    return a / b;
+}
+
+console.log(add(5,7));
+console.log(subtract(8,4));
+console.log(multiply(3,9));
+console.log(divide(12,3));
+
+function operate(a,b,opType){
+    return opType(a,b);
+}
+console.log(operate(5,7,add));
+console.log(operate(8,4,subtract));
+console.log(operate(3,9, multiply));
+console.log(operate(12,3,divide));
